2961731e04e6dcb18fabac22d6e2d93d7cb675bc
kelvinsantos <kelvin.santos@tooltwist.com>
2018-10-10 17:53:45 +0800

    make it better

--- src/components/RoyalRumble.vue
+++ src/components/RoyalRumble.vue
@@ -26,20 +26,29 @@ export default {
         'Elizabeth I',
         'William I'
       ],
+      tagsTmpContainer: [],
       sortedTags: []
     }
   },
   methods: {
     getSortedList: function() {
-      this.sortedTags = this.tags.map((n) => ({name: n, num: this.romanToNumber(n.split(" ").pop())}))
-      .sort((a, b) => {
-        if (a.name < b.name) 
-          return -1
-        if (a.name > b.name) 
-          return 1
-        return 0
+      // clear the tags container
+      this.tagsTmpContainer = []
+
+      // Intl.Collator
+      var collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+      this.tags.map((n) => {
+        this.tagsTmpContainer.push(n.split(" ")[0] + '_' + this.romanToNumber(n.split(" ").pop()))
       })
-      .map(({name, num}) => name)
+
+      setTimeout(() => {
+        this.tagsTmpContainer.sort(collator.compare)
+      }, 1000)
+
+      setTimeout(() => {
+        this.sortedTags = this.tagsTmpContainer.map((n) => ({name: n.split('_')[0], num: this.numberToRoman(n.split('_')[1])}))
+        .map(({name, num}) => name + ' ' + num)
+      }, 2000)
     },
     romanToNumber: function(roman) {
       if (roman === "")           
@@ -59,6 +68,19 @@ export default {
       if (roman.startsWith("I"))  
         return 1  + this.romanToNumber(roman.substr(1));
       return 0;
+    },
+    numberToRoman: function(num) {
+      if (isNaN(num))
+        return NaN;
+      var digits = String(+num).split(""),
+          key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                "","I","II","III","IV","V","VI","VII","VIII","IX"],
+          roman = "",
+          i = 3;
+      while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman
+      return Array(+digits.join("") + 1).join("M") + roman
     }
   }
 }
